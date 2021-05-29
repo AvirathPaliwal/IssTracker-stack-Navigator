21959fbfd5b95b080bec06f3b51d7f7e7d62bbf2
AvirathPaliwal <73025697+AvirathPaliwal@users.noreply.github.com>
2021-05-29 16:52:45 +0530

    Add files via upload

---
+++ screens/IssLocation.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import {View,Text} from 'react-native'
+
+export default class IssLocationScreen extends Component{
+    render(){
+        return(
+            <View>
+               <Text>Iss Live Location</Text>
+
+            </View>
+        )
+    }
+}
